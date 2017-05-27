945f464c5f15f62acb39357ac77a2d142e4c71c3
jenbuska <joona.enbuska@eficode.com>
2017-05-27 13:12:27 +0300

    remap state to props on props changes

--- src/connect.jsx
+++ src/connect.jsx
@@ -33,17 +33,24 @@ const connector = (Component, mapStateToProps = store => store.state, mapDispatc
 
     shouldComponentUpdate(nextProps, nextState) {
       const { state, props, } = this;
-      const propsChanges = keys({ ...props, ...nextProps, }).filter(k => props[k] !== nextProps[k]);
-      if (propsChanges.length) {
-        const { store, } = this.context;
+      const dispatchChanges= keys({ ...props, ...nextProps, }).filter(k => props[k] !== nextProps[k]);
+      const mapStateChanges = keys({ ...state, ...nextState, }).filter(k => props[k] !== nextProps[k]);
+      let shouldUpdate;
+      const { store, } = this.context;
+      if (dispatchChanges.length) {
         this.mapDispatchToProps = entries(mapDispatchToProps)
           .reduce(function (acc, [ key, value, ]) {
-            acc[key] = acc[key] = (...params) => value(...params)(store, props)
+            acc[key] = acc[key] = (...params) => value(...params)(store, props);
             return acc;
           }, {});
-        return true;
+        shouldUpdate= true;
       }
-      return keys({ ...state, ...nextState, }).some(k => state[k] !== nextState[k]);
+      if (mapStateChanges.length) {
+        nextState = mapStateToProps(store.state, this.props);
+        this.setState(nextState);
+        shouldUpdate= true;
+      }
+      return shouldUpdate;
     }
 
     componentWillUnmount() {
