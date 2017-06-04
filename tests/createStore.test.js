69c02d0caadb5e245611c346401594698ca6f503
jenbuska <joona.enbuska@eficode.com>
2017-06-04 15:35:11 +0300

    rename createStore test to createStore.test so that the tests will be run

---
+++ tests/createStore.test.js
@@ -0,0 +1,24 @@
+import { expect, } from 'chai';
+import createStore from '../src/createStore';
+
+describe('Create store', () => {
+  let root;
+  it('should return 1 level initial values', () => {
+    root = createStore({ a: 1, });
+    expect(root.state).to.deep.equal({ a: 1, });
+  });
+
+  it('should return 2 level initial values', () => {
+    root = createStore({ a: 1, b: { c: 1, }, });
+    expect(root.state).to.deep.equal({ a: 1, b: { c: 1, }, });
+  });
+
+  it('should return 3 level initial values', () => {
+    root = createStore({ a: 1, b: { c: 2, d: 3, e: { f: 4, }, }, });
+    expect(root.state).to.deep.equal({ a: 1, b: { c: 2, d: 3, e: { f: 4, }, }, });
+  });
+  it('should be able to reference children', () => {
+    root = createStore({ a: 1, b: { c: 2, d: 3, e: { f: 4, }, }, });
+    root.b.c;
+  })
+});
