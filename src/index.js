cd32ed1a4ba0746fac188f42df0a4e48199af78d
jenbuska <joona.enbuska@eficode.com>
2017-06-15 18:51:00 +0300

    export shapes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import Provider from './Provider';
 import connect from './connect';
+import { TYPE, TYPE_ANY, TARGET_ANY, VALIDATE, } from './createStore';
 
-export { Provider, connect, };
+const shapes = { TYPE, TYPE_ANY, TARGET_ANY, VALIDATE, };
+export { Provider, connect, shapes, };
