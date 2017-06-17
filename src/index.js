e4a2dc01db4112d8355c7f43c36225dca5cf8228
jenbuska <joona.enbuska@eficode.com>
2017-06-17 20:14:39 +0300

    make SubStore available from default impport

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Provider from './Provider';
 import connect from './connect';
+import SubStore from './SubStore';
 import { string, number, spec, any, object, array, exclusive, isRequired, none, bool, } from './createStore';
 
 const shapes = { string, number, spec, any, object, array, exclusive, isRequired, none, bool, };
-export { Provider, connect, shapes, };
+export { Provider, connect, shapes, SubStore, };
