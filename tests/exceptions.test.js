d03b71d08a4d847ce0302111ab6b29bd31220261
jenbuska <joona.enbuska@eficode.com>
2017-06-18 13:27:34 +0300

    extend expection handling tests

--- tests/exceptions.test.js
+++ tests/exceptions.test.js
@@ -1,6 +1,17 @@
 import { expect, } from 'chai';
 import createStore, { StoreCreator, } from '../src/createStore';
 
+function verifyErrorOnChange(...params) {
+  params.forEach(next => {
+    expect(() => next.setState(1)).to.throw(Error);
+    expect(() => next.setState({ x: 100, })).to.throw(Error);
+    expect(() => next.clearState(1)).to.throw(Error);
+    expect(() => next.clearState({ x: 100, })).to.throw(Error);
+    expect(() => next.remove()).to.throw(Error);
+    expect(() => next.remove('b')).to.throw(Error);
+  });
+}
+
 describe('killSwitch', () => {
   let store;
   it('Kill switch should trigger when depth > 100 ',
@@ -17,32 +28,21 @@ describe('killSwitch', () => {
       ref.setState({ a: {}, });
       expect(killSwitchIsTriggered).to.be.ok;
     });
-  it('changing detached sub store should throw an exception',
+  it('changing removed sub store should throw an exception',
     () => {
       store = createStore({ a: 1, b: 2, c: { d: 3, }, });
       const { a, c, } = store;
       const { d, } = c;
       store.remove('a');
       store.c.remove();
-      expect(() => a.setState(1)).to.throw(Error);
-      expect(() => a.setState({ x: 100, })).to.throw(Error);
-      expect(() => a.clearState(1)).to.throw(Error);
-      expect(() => a.clearState({ x: 100, })).to.throw(Error);
-      expect(() => a.remove()).to.throw(Error);
-      expect(() => a.remove('b')).to.throw(Error);
-
-      expect(() => c.setState(1)).to.throw(Error);
-      expect(() => c.setState({ x: 100, })).to.throw(Error);
-      expect(() => c.clearState(1)).to.throw(Error);
-      expect(() => c.clearState({ x: 100, })).to.throw(Error);
-      expect(() => c.remove()).to.throw(Error);
-      expect(() => c.remove('b')).to.throw(Error);
-
-      expect(() => d.setState(1)).to.throw(Error);
-      expect(() => d.setState({ x: 100, })).to.throw(Error);
-      expect(() => d.clearState(1)).to.throw(Error);
-      expect(() => d.clearState({ x: 100, })).to.throw(Error);
-      expect(() => d.remove()).to.throw(Error);
-      expect(() => d.remove('b')).to.throw(Error);
+      verifyErrorOnChange(a, c, d);
     });
+
+  it('changing excluded sub store should throw an exception', () => {
+    store = createStore({ a: 1, b: 2, c: { d: 3, }, });
+    const { a, c, } = store;
+    const { d, } = c;
+    store.clearState({ b: 2, });
+    verifyErrorOnChange(a, c, d);
+  });
 });
