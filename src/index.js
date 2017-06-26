4012c38230f86b1e098180d2a98cba002fa45115
jenbuska <joona.enbuska@eficode.com>
2017-06-26 17:28:16 +0300

    export SubStoreLeaf from index

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import Provider from './Provider';
 import connect from './connect';
 import SubStore from './SubStore';
 import createStore from './createStore';
+import SubStoreLeaf from './SubStoreLeaf';
 import * as shapes from './shape';
 
-export { Provider, connect, shapes, SubStore, createStore, };
+export { Provider, connect, shapes, SubStore, createStore, SubStoreLeaf, };
