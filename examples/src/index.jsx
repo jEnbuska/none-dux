356649757ece9148eb9c94286ba5b421d6d8208c
jenbuska <joona.enbuska@eficode.com>
2017-06-30 15:50:26 +0300

    use jsx definition

--- examples/src/index.jsx
+++ examples/src/index.jsx
@@ -2,83 +2,73 @@ import 'styles';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory, IndexRedirect, } from 'react-router';
-import { Provider, shapes, } from '../../src';
+import { Definition, Str, Numb, Err, Bool, Obj, } from '../../src/shape/index';
+import Provider from '../../src/Provider';
 import UserProfile from './containers/UserProfile.jsx';
 import BrowseUsers from './containers/BrowseUsers.jsx';
 import Users from './containers/Users.jsx';
 import App from './components/App';
 
-const initialState= {
-  users: { content: {}, status: {}, },
-  todosByUser: { content: {}, status: {}, },
-  selections: { user: {}, },
-};
-
-const { spec, anyKey, anyValue, array, object, number, string, exclusive, isRequired, bool, } = shapes;
-
 // If you specify invalid shape to provider, console errors will try to provided information to fix the problem
 // Note that shape is not used when NODE_ENV==='production '
 
-const shape = {
-  [spec]: { object, exclusive, },
-  users: { [spec]: { object, isRequired, },
-    status: { [spec]: { object, isRequired, },
-      pending: { [spec]: { bool, }, },
-      error: { [spec]: { anyValue, }, },
-    },
-    content: { [spec]: { object, isRequired, },
-      [anyKey]: { [spec]: { object, exclusive, }, // users [anyKey] means any key, in this case its userId uuid
-        id: { [spec]: { string, isRequired, }, },
-        firstName: { [spec]: { string, isRequired, }, },
-        lastName: { [spec]: { string, isRequired, }, },
-        email: { [spec]: { string, }, },
-        phone: { [spec]: { string, isRequired, }, },
-        age: { [spec]: { number, }, },
-        single: { [spec]: { bool, }, },
-        pending: { [spec]: { bool, }, },
-      },
-    },
-
-  },
-  todosByUser: {
-    [spec]: { object, isRequired, exclusive, },
-    status: {
-      [spec]: { object, isRequired, exclusive, },
-      pending: { [spec]: { bool, }, },
-      error: { [spec]: { anyValue, }, },
-    },
-    content: {
-      [spec]: { object, isRequired, },
-      [anyKey]: {
-        [spec]: { object, },
-        [anyKey]: {
-          [spec]: { object, exclusive, },
-          id: { [spec]: { string, isRequired, }, },
-          userId: { [spec]: { string, isRequired, }, },
-          description: { [spec]: { string, isRequired, }, },
-          done: { [spec]: { bool, }, },
-          pending: { [spec]: { bool, }, },
-        },
-      },
-    },
-  },
-  selections: { [spec]: { object, exclusive, },
-    user: { [spec]: { object, },
-      firstName: { [spec]: { string, }, },
-      lastName: { [spec]: { string, }, },
-      email: { [spec]: { string, }, },
-      phone: { [spec]: { string, }, },
-      age: { [spec]: { number, }, },
-      single: { [spec]: { bool, }, },
-    },
-  },
-};
+const definition = (
+  <Definition>
+    <Obj name='users' initial isRequired>
+      <Obj name='status' initial isRequired>
+        <Bool name='pending' />
+        <Err name='error' />
+      </Obj>
+      <Obj name='content' initial isRequired>
+        <Obj many>
+          <Str name='id' />
+          <Str name='firstName' />
+          <Str name='lastName' />
+          <Str name='email' />
+          <Str name='phone' />
+          <Str name='phone' />
+          <Numb name='age' />
+          <Bool name='single' />
+          <Bool name='pending' />
+        </Obj>
+      </Obj>
+    </Obj>
+    <Obj name='todosByUser' initial isRequired>
+      <Obj name='status' initial isRequired>
+        <Bool name='pending' />
+        <Err name='error' />
+      </Obj>
+      <Obj name='content' initial isRequired>
+        <Obj many> {/* by user id*/}
+          <Obj many> {/* by todo id*/ }
+            <Str name='id' />
+            <Str name='userId' />
+            <Str name='description' />
+            <Bool name='done' />
+            <Bool name='pending' />
+          </Obj>
+        </Obj>
+      </Obj>
+    </Obj>
+    <Obj name='selections' initial isRequired>
+      <Obj name='user' initial isRequired>
+        <Str name='id' />
+        <Str name='firstName' />
+        <Str name='lastName' />
+        <Str name='email' />
+        <Str name='phone' />
+        <Numb name='age' />
+        <Bool name='single' />
+        <Bool name='pending' />
+      </Obj>
+    </Obj>
+  </Definition>
+);
 
 const Root = () => (
   <Provider
-    initialState={initialState}
     onChange={(store, lastChange) => {}}
-    shape={shape}>
+    definition={definition}>
     <Router history={browserHistory}>
       <Route path='/' component={App}>
         <IndexRedirect to='users' />
