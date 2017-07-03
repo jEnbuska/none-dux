41facf4a7acc98be3903378915ea3836738d5e5f
jenbuska <joona.enbuska@eficode.com>
2017-07-03 13:01:34 +0300

    fix subStore array leaf

--- src/SubStoreLeaf.js
+++ src/SubStoreLeaf.js
@@ -1,4 +1,4 @@
-const { assign, freeze, seal, } = Object;
+const { assign, freeze, seal, values, } = Object;
 
 // If you have an object that you be an SubStore
 /*
@@ -22,13 +22,103 @@ export class SubStoreObjectLeaf {
     freeze(this);
     seal(this);
   }
-
 }
 
-export class SubStoreArrayLeaf extends Array {
-  constructor(arr) {
-    super(...arr);
+function onThrow(func) {
+  throw new Error('SubStoreArrayLeaf does not implement "'+ func+'"');
+}
+
+export class SubStoreArrayLeaf {
+  constructor(arr = []) {
+    this.length = arr.length;
+    assign(this, arr);
     freeze(this);
     seal(this);
+  }
+
+  values() {
+    const { length, ...rest } = this;
+    return Object.values(rest);
+  }
+
+  map(func) {
+    return this.values().map(func);
+  }
+
+  filter(arg) {
+    return this.values().filter(arg);
+  }
+
+  concat() {
+    onThrow('concat');
+  }
+  copyWithin() {
+    onThrow('copyWithin');
+  }
+  every(arg) {
+    return this.values().every(arg);
+  }
+  fill() {
+    onThrow('fill');
+  }
+  find(arg) {
+    return this.values().find(arg);
+  }
+  findIndex(arg) {
+    return this.values().findIndex(arg);
+  }
+  forEach(arg) {
+    return this.values().forEach(arg);
+  }
+  indexOf(arg) {
+    return this.values().indexOf(arg);
+  }
+  isArray() {
+    onThrow('isArray');
+  }
+  join(arg) {
+    return this.values().join(arg);
+  }
+  lastIndexOf(arg) {
+    return this.values().lastIndexOf(arg);
+  }
+  pop() {
+    onThrow('pop');
+  }
+  push() {
+    onThrow('push');
+  }
+  reduce(arg, seed) {
+    return this.values().reduce(arg, seed);
+  }
+  reduceRight(arg, seed) {
+    return this.values().reduceRight(arg, seed);
+  }
+  reverse() {
+    onThrow('reverse');
+  }
+  shift() {
+    onThrow('shift');
+  }
+  slice(arg1, arg2) {
+    return this.values().slice(arg1, arg2);
+  }
+  some(arg) {
+    return this.values().some(arg);
+  }
+  sort() {
+    onThrow('sort');
+  }
+  splice() {
+    onThrow('splice');
+  }
+  toString()	{
+    return this.values().toString();
+  }
+  unshift() {
+    onThrow('unshift');
+  }
+  valueOf() {
+    return this.values().valueOf();
   }
 }
--- tests/subStoreLeafs.test.js
+++ tests/subStoreLeafs.test.js
@@ -1,21 +1,32 @@
 
 import nonedux from '../src/createNoneDux';
-import createLeaf from '../src/SubStoreLeaf';
+import createLeaf, { SubStoreObjectLeaf, SubStoreArrayLeaf, } from '../src/SubStoreLeaf';
 
-describe('SubStoreLeaf', () => {
-  test('creating subject with SubStoreObjectLeaf', () => {
-    const { subject, } = nonedux({ a: { b: createLeaf({ c: 1, d: 2, }), }, });
-    expect(subject.state).toEqual({ a: { b: { c: 1, d: 2, }, }, });
-    expect(subject.a.b).toBe(undefined);
-    expect(subject.a.state).toEqual({ b: { c: 1, d: 2, }, });
+describe('Leafs', () => {
+  test('create substore array leaf', () => {
+    const leaf = createLeaf([ 1, 2, { a: 3, b: { c: 4, }, }, 'd', ]);
+    const acc = {};
+    leaf.forEach((v, i) => acc[i]=v);
+    expect(acc).toEqual({ 0: 1, 1: 2, 2: { a: 3, b: { c: 4, }, }, 3: 'd', });
+    const [ ...arr ] = leaf;
+    expect(arr).toEqual([ 1, 2, { a: 3, b: { c: 4, }, }, 'd', ]);
+    expect(leaf.reduce((acc, next, i) => Object.assign(acc, { [i]: next, }), {})).toEqual({ 0: 1, 1: 2, 2: { a: 3, b: { c: 4, }, }, 3: 'd', });
+    expect(leaf.map(it => it)).toEqual([ 1, 2, { a: 3, b: { c: 4, }, }, 'd', ]);
+    expect(leaf.filter((n, i) => i%2===0)).toEqual([ 1, { a: 3, b: { c: 4, }, }, ]);
+    expect(leaf.every((n, i) => Number.isInteger(i))).toEqual(true);
+    expect(leaf.every((n, i) => Number.isInteger(n))).toEqual(false);
+    expect(leaf.some((n, i) => Number.isInteger(n))).toEqual(true);
+    expect(leaf.some((n, i) => n instanceof Array)).toEqual(false);
   });
 
-  test('creating subject with SubStoreArrayLeaf', () => {
-    const { subject, } = nonedux({ a: { b: createLeaf([ 1, 2, 3, 4, ]), }, });
-    expect(subject.state).toEqual({ a: { b: [ 1, 2, 3, 4, ], }, });
-    expect(subject.a.state).toEqual({ b: [ 1, 2, 3, 4, ], });
-    expect(subject.a.b.state).toEqual([ 1, 2, 3, 4, ]);
-    expect(subject.a.b[0]).toBe(undefined);
+  test('creating subject with SubStoreObjectLeaf & SubStoreArrayLeafs', () => {
+    const { subject, } = nonedux({ a: { b: createLeaf({ c: { x: 1, }, d: 2, }), e: createLeaf([ { f: 1, }, 2, ]), }, });
+    expect(subject.state).toEqual({ a: { b: new SubStoreObjectLeaf({ c: { x: 1, }, d: 2, }), e: new SubStoreArrayLeaf([ { f: 1, }, 2, ]), }, });
+    expect(subject.a.state).toEqual({ b: new SubStoreObjectLeaf({ c: { x: 1, }, d: 2, }), e: new SubStoreArrayLeaf([ { f: 1, }, 2, ]), });
+    expect(subject.a.b).toBeUndefined();
+    expect(subject.a.e).toBeUndefined();
+    expect(subject.a.state.b).toEqual({ c: { x: 1, }, d: 2, });
+    expect(subject.a.state.e).toEqual(new SubStoreArrayLeaf([ { f: 1, }, 2, ]));
   });
 
   test('setState with SubStoreObjectLeaf', () => {
@@ -23,11 +34,11 @@ describe('SubStoreLeaf', () => {
     expect(subject.state).toEqual({ a: { b: { }, }, });
     expect(subject.a.b.state).toEqual({});
     subject.a._onSetState({ b: createLeaf({ c: 1, d: 2, }), });
-    expect(subject.a.b).toBe(undefined);
+    expect(subject.a.b).toBeUndefined();
     expect(subject.a.state).toEqual({ b: { c: 1, d: 2, }, });
     subject.a._onSetState({ e: createLeaf({ f: 3, g: 4, }), });
-    expect(subject.a.b).toBe(undefined);
-    expect(subject.a.e).toBe(undefined);
+    expect(subject.a.b).toBeUndefined();
+    expect(subject.a.e).toBeUndefined();
     expect(subject.a.state).toEqual({ b: { c: 1, d: 2, }, e: { f: 3, g: 4, }, });
   });
 
@@ -36,7 +47,7 @@ describe('SubStoreLeaf', () => {
     expect(subject.state).toEqual({ a: { b: {}, c: 2, d: {}, }, });
     expect(subject.a.b.state).toEqual({});
     subject.a._onClearState({ b: createLeaf({ c: 1, d: 2, }), });
-    expect(subject.a.b).toBe(undefined);
+    expect(subject.a.b).toBeUndefined();
     expect(subject.a.state).toEqual({ b: { c: 1, d: 2, }, });
   });
 
@@ -49,7 +60,7 @@ describe('SubStoreLeaf', () => {
   test('Add Error type', () => {
     const err = new Error('some error');
     const { subject, } = nonedux({ a: { err, }, });
-    expect(subject.a.err).toBe(undefined);
+    expect(subject.a.err).toBeUndefined();
     expect(subject.a.state.err).toEqual(err);
   });
 });
