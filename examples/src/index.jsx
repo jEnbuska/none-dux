a9fec31dfbfa6edc861b9d21c043be62368d780a
jenbuska <joona.enbuska@eficode.com>
2017-07-05 18:42:03 +0300

    use shape validator

--- examples/src/index.jsx
+++ examples/src/index.jsx
@@ -4,7 +4,8 @@ import { Provider, } from 'react-redux';
 import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory, IndexRedirect, } from 'react-router';
 import { applyMiddleware, createStore, } from 'redux';
-import nonedux, { shapes, } from '../../src';
+import nonedux, { validatorMiddleware, } from '../../src';
+import shape from './shape';
 import UserProfile from './containers/UserProfile.jsx';
 import BrowseUsers from './containers/BrowseUsers.jsx';
 import Users from './containers/Users.jsx';
@@ -16,68 +17,9 @@ const initialState= {
   selections: { user: {}, },
 };
 
-const { spec, anyKey, anyValue, object, number, string, exclusive, isRequired, bool, } = shapes;
+const { reducer, thunk, subject, } = nonedux(initialState);
 
-// If you specify invalid shape to provider, console errors will try to provided information to fix the problem
-// Note that shape is not used when NODE_ENV==='production '
-
-const shape = {
-  [spec]: { object, exclusive, },
-  users: { [spec]: { object, isRequired, },
-    status: { [spec]: { object, isRequired, },
-      pending: { [spec]: { bool, }, },
-      error: { [spec]: { anyValue, }, },
-    },
-    content: { [spec]: { object, isRequired, },
-      [anyKey]: { [spec]: { object, exclusive, }, // users [anyKey] means any key, in this case its userId uuid
-        id: { [spec]: { string, isRequired, }, },
-        firstName: { [spec]: { string, isRequired, }, },
-        lastName: { [spec]: { string, isRequired, }, },
-        email: { [spec]: { string, }, },
-        phone: { [spec]: { string, isRequired, }, },
-        age: { [spec]: { number, }, },
-        single: { [spec]: { bool, }, },
-        pending: { [spec]: { bool, }, },
-      },
-    },
-
-  },
-  todosByUser: {
-    [spec]: { object, isRequired, exclusive, },
-    status: {
-      [spec]: { object, isRequired, exclusive, },
-      pending: { [spec]: { bool, }, },
-      error: { [spec]: { anyValue, }, },
-    },
-    content: {
-      [spec]: { object, isRequired, },
-      [anyKey]: {
-        [spec]: { object, },
-        [anyKey]: {
-          [spec]: { object, exclusive, },
-          id: { [spec]: { string, isRequired, }, },
-          userId: { [spec]: { string, isRequired, }, },
-          description: { [spec]: { string, isRequired, }, },
-          done: { [spec]: { bool, }, },
-          pending: { [spec]: { bool, }, },
-        },
-      },
-    },
-  },
-  selections: { [spec]: { object, exclusive, },
-    user: { [spec]: { object, },
-      firstName: { [spec]: { string, }, },
-      lastName: { [spec]: { string, }, },
-      email: { [spec]: { string, }, },
-      phone: { [spec]: { string, }, },
-      age: { [spec]: { number, }, },
-      single: { [spec]: { bool, }, },
-    },
-  },
-};
-
-const { reducer, thunk, } = nonedux(initialState, shape);
-const createStoreWithMiddleware = applyMiddleware(...[ thunk, ])(createStore);
+const createStoreWithMiddleware = applyMiddleware(...[ thunk, validatorMiddleware(subject, shape), ])(createStore);
 const store = createStoreWithMiddleware(reducer, window.devToolsExtension && window.devToolsExtension());
 
 const Root = () => (
---
+++ examples/src/shape.js
@@ -0,0 +1,56 @@
+import { shape, } from '../../src';
+
+const { types, any, } = shape;
+const { string, strict, number, bool, isRequired, } = types;
+export default {
+  ...strict.isRequired,
+  users: {
+    ...strict.isRequired,
+    content: {
+      ...isRequired.strict,
+      [any]: {
+        id: string.isRequired,
+        firstName: string.isRequired,
+        lastName: string.isRequired,
+        email: string.isRequired,
+        age: number.isRequired,
+        single: bool,
+      },
+      pending: bool,
+    },
+    status: {
+      ...isRequired.strict,
+      pending: bool,
+      error: bool,
+    },
+  },
+  todosByUser: {
+    ...strict.isRequired,
+    content: {
+      [any]: {
+        [any]: {
+          ...strict,
+          id: string.isRequired,
+          userId: string.isRequired,
+          description: string.isRequired,
+          done: bool.isRequired,
+          pending: bool,
+        },
+      },
+    }, status: {
+      pending: bool,
+    }, },
+  selections: {
+    ...strict,
+    user: {
+      ...strict,
+      id: string,
+      firstName: string,
+      lastName: string,
+      email: string,
+      age: number,
+      phone: string,
+      single: bool,
+      pending: bool,
+    }, },
+};
