1d5d45a280037409df9fc42c13be951664639904
jenbuska <joona.enbuska@eficode.com>
2017-07-05 20:56:33 +0300

    update README

--- src/shape/index.js
+++ src/shape/index.js
@@ -1,5 +1,6 @@
 import { any, } from './createValidator';
 import validatorMiddleware from './createValidatorMiddleware';
 import types from './types';
+import { spec, } from './Validator';
 
-export default { validatorMiddleware, any, types, };
+export default { validatorMiddleware, any, types, spec, };
