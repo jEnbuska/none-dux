81482d9422f2f51bdcdfd61d8fb60a22e0f8a586
jenbuska <joona.enbuska@eficode.com>
2017-07-07 13:27:45 +0300

    make shapevalidator middleware easier to test

--- src/shape/createValidatorMiddleware.js
+++ src/shape/createValidatorMiddleware.js
@@ -2,13 +2,6 @@ import { spec, } from './Validator';
 import createValidator, { any, } from './createValidator';
 import { naturalLeafTypes, checkers, } from './types';
 
-const emptyShape = {
-  [spec]: {
-    name: 'Object',
-    isRequired: false,
-    strict: false,
-  },
-};
 function stringify(obj) {
   try {
     return JSON.stringify(obj, null, 2);
@@ -25,12 +18,39 @@ function performValidation() {
   pending=undefined;
 }
 
+export const onErrorHandlers = {
+  onStrictError: (identity, key, state) =>
+    console.error('"strict" validation failed:' +
+    '\nAt: "'+identity.join(', ')+'"' +
+    '\nNo validations for key: '+key +'' +
+    '\nWith value: '+ stringify(state)),
+  onRequiredError: (identity, key) =>
+    console.error('"isRequired" validation failed:' +
+    '\nAt: '+identity.join(', ')+'"' +
+    '\nIs missing value for key: '+key),
+  onTypeError: (isRequired, strict, type, state, identity) =>
+    console.error('Validation failed at "'+identity.join(', ')+'"\n' +
+    'Expected: '+ type+'' +
+    '\nisRequired: '+ isRequired + '' +
+    '\nstrict: '+strict +
+    '\nBut got '+stringify(state)),
+};
+
+const emptyShape = {
+  [spec]: {
+    name: 'Object',
+    isRequired: false,
+    strict: false,
+  },
+};
+
 export default function createValidatorMiddleware(subject, shape = emptyShape) {
   shape = createValidator(shape);
+  console.log(JSON.stringify({shape}, null, 2))
   subject.__substore_parent__._notifyUp = () => {
     performValidation();
   };
-  validateRecursively(subject.state, subject.prevState, subject.getIdentity(), shape);
+  validateRecursively(subject.state, subject.prevState, subject.getIdentity(), shape, true);
   return () => (next) => (action) => {
     const result = next(action);
     const { target, } = action;
@@ -51,55 +71,41 @@ export default function createValidatorMiddleware(subject, shape = emptyShape) {
 }
 
 const { keys, getPrototypeOf, } = Object;
-const { error, } = console;
-function validateRecursively(state, prevState, identity, shape) {
-  const { [spec]: specification, [any]: anyK, ...children } = shape;
+function validateRecursively(state, prevState, identity, shape, initial) {
+  const { [spec]: specification, [any]: _, ...children } = shape;
   const { name, strict, isRequired, } = specification || {};
-
   if (checkers[name](state)) {
     if (state && !naturalLeafTypes[getPrototypeOf(state).constructor.name]) {
       keys({ ...state, ...children, })
       .filter((k) => {
         try {
-          return state[k] !== prevState[k];
+          return initial || state[k] !== prevState[k];
         } catch (Exception) {
           return true;
         }
       })
       .forEach((k) => {
-        if (strict && !shape[k] && !anyK) {
-          error('"strict" validation failed:' +
-            '\nAt: "'+identity.join(', ')+'"' +
-            '\nNo validations for key: '+k+'' +
-            '\nWith value: '+ stringify(state[k]));
-        } else if (shape[k]) {
-          if (shape[k][spec].isRequired && !state.hasOwnProperty(k)) {
-            error('"isRequired" validation failed:' +
-              '\nAt: '+identity.join(', ')+'"' +
-              '\nIs missing value for key: '+k);
+        const subShape = shape[k] || shape[any];
+        if (!subShape && strict) {
+          onErrorHandlers.onStrictError(identity, k, state[k]);
+        } else if (subShape) {
+          if (state[k] === null || state[k] === undefined) {
+            if (subShape[spec].isRequired) {
+              onErrorHandlers.onRequiredError(identity, k);
+            }
           } else {
             validateRecursively(
-              state && state.hasOwnProperty(k) ? state[k] : undefined,
+              state[k],
               prevState && prevState.hasOwnProperty(k) ? prevState[k] : undefined,
               [ ...identity, k, ],
-              shape[k]
+              subShape,
+              initial,
             );
           }
-        } else if (anyK) {
-          validateRecursively(
-            state && state.hasOwnProperty(k) ? state[k] : undefined,
-            prevState && prevState.hasOwnProperty(k) ? prevState[k] : undefined,
-            [ ...identity, k, ],
-            shape[any],
-          );
         }
       });
     }
-  } else if (isRequired || (state !== undefined && state!==null)) {
-    error('Validation failed at "'+identity.join(', ')+'"\n' +
-      'Expected: '+ name+'' +
-      '\nisRequired: '+ isRequired + '' +
-      '\nstrict: '+strict +
-      '\nBut got '+stringify(state));
+  } else {
+    onErrorHandlers.onTypeError(isRequired, strict, name, state, identity);
   }
 }
