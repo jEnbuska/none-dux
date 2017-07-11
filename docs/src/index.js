8485ec4e7706c88c086669c316e91cd7dfb7d71c
jenbuska <joona.enbuska@eficode.com>
2017-07-11 17:14:37 +0300

    update docs

--- docs/src/index.js
+++ docs/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 
+console.log('should log 4')
 ReactDOM.render(
   <BrowserRouter>
     <App />
