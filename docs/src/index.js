3f2a4825bf7960a51f43fc02dd18287dc6ee6ca3
jenbuska <joona.enbuska@eficode.com>
2017-07-12 11:31:04 +0300

    update docs

--- docs/src/index.js
+++ docs/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, } from 'react-router-dom';
+import { BrowserRouter, Route} from 'react-router-dom';
 import { Provider, } from 'react-redux';
 import { applyMiddleware, createStore, } from 'redux';
 import nonedux from 'none-dux';
@@ -20,7 +20,7 @@ console.log('should log 4');
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <Route path='/' component={App}/>
     </BrowserRouter>
   </Provider>,
   document.getElementById('root'),
