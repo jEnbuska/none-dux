f24faf3ca9d4b45bf6b646aff3a0fd1c6a79660d
jenbuska <joona.enbuska@eficode.com>
2017-07-17 21:24:09 +0300

    change conflicting item name in localstorage

--- examples/sagaExample/src/mockClient.js
+++ examples/sagaExample/src/mockClient.js
@@ -4,7 +4,7 @@ export default {
   storeItem: ({ token, }) => localStorage.setItem('token', token),
   clearItem: (key) => localStorage.removeItem(key),
   login: ({ email, password, }) => {
-    let users = localStorage.getItem('users');
+    let users = localStorage.getItem('saga_users');
     let user;
     if (users) {
       users = parse(users);
@@ -14,7 +14,7 @@ export default {
   },
 
   signup: ({ email, password, firstname, lastname, zip, city, address, phone, }) => {
-    let users = localStorage.getItem('users');
+    let users = localStorage.getItem('saga_users');
     let preExisting;
     if (users) {
       users = parse(users);
@@ -25,7 +25,7 @@ export default {
       preExisting =true;
     } else {
       users[email] = { [password]: { email, password, firstname, lastname, zip, city, address, phone, }, };
-      localStorage.setItem('users', stringify(users));
+      localStorage.setItem('saga_users', stringify(users));
     }
     return new Promise((resolve, reject) => {
       setTimeout(() => preExisting ? reject({ msg: 'email already taken', }) : resolve({ token: 'abc', }), 1200);
@@ -36,12 +36,12 @@ export default {
 
   timeUntilTokenExpiration: 60*60*1000,
   onTermsAccepted: ({ email, }) => {
-    const users = parse(localStorage.getItem('users'));
+    const users = parse(localStorage.getItem('saga_users'));
     let user = users[email];
     const pass = Object.keys(users[email])[0]
     user = user[pass]
     user.termsAccepted = true;
-    localStorage.setItem('users', stringify(users));
+    localStorage.setItem('saga_users', stringify(users));
     return new Promise(resolve => setTimeout(() => resolve(), 800));
   },
   getToken() {
