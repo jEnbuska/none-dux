4e5058e7293b9edbb22ccf6190d5593876ba5107
jenbuska <joona.enbuska@eficode.com>
2017-08-05 09:12:32 +0300

    further test actioncreators

--- tests/actionCreator.test.js
+++ tests/actionCreator.test.js
@@ -2,21 +2,27 @@ import { createStoreWithNonedux, } from './utils';
 
 function actionCreator(apply) {
   return function (dux) {
-    return apply(dux);
+    apply(dux);
   };
 }
 
-describe('using action creators', async () => {
+describe('using action creators', () => {
   [ 'legacy', 'proxy', ].forEach(name => {
     const init = state => createStoreWithNonedux(state, undefined, undefined, name==='proxy');
-    describe('run ' + name +' configuration', async () => {
-      test('using action creator', async () => {
+    describe('run ' + name +' configuration', () => {
+      test('should be able to dispatch action creator actions after clearReferences', () => {
         const { store, } = init({ a: { b: {}, }, });
         store.dispatch(actionCreator(dux => dux.a.setState({ b: { c: {}, d: {}, }, })));
-        await new Promise(res => setTimeout(() => {
+        return new Promise(res => {
           expect(() => store.dispatch(actionCreator(dux => dux.a.b.remove('c')))).not.toThrow();
           res();
-        }, 0));
+        });
+      });
+
+      test('all action creators should return a promise', () => {
+        const { store, } = init({ a: { b: {}, }, });
+        const res = store.dispatch(actionCreator(dux => dux.a.setState({ b: { c: {}, d: {}, }, })));
+        expect(res).toBeInstanceOf(Promise);
       });
     });
   });
